c7348fe01eeb6c245399116a83ea4d26d73699a7
vahorailsha4-dotcom <vahorailsa4@gmail.com>
2026-06-23 12:46:19 +0530

    ilsa

--- day3/task4_tuple.py
+++ day3/task4_tuple.py
@@ -1,8 +1,16 @@
-sub={"vahorailsha4@gmail.com","vahorailsa722@gamil.com"}
-cust={"vahorailsha4@gmail.com","vahorailsa722@gamil.com"}
+subscribers={
+    "a@gmail.com"
+    "b@gmail.com"
+    "c@gmail.com"
+}
 
-Purchased=sub_cust
-sub1=cust-sub
+customers={
+    "d@gmail.com"
+    "e@gmail.com"
+    "f@gmail.com"
+}
+never_purchsed=subscribers-customers
+purchased_not_subscribed=customers-subscribers
 
-print("Email of the Subscribers never Purcased:",purcased)
-print("Email of the customer never subscriber:",sub1)
+print("subscribers who never purchased:",never_purchsed)
+print("customers who purchased but not subscribed:",purchased_not_subscribed)
